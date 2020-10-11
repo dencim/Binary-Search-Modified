0895de0c1f96cf8659c51ba12b30d7566e4c1ab3
dencim <43787288+dencim@users.noreply.github.com>
2020-10-11 16:23:48 -0400

    Bug fixes for list shifted more then k/2 times

--- Part3.py
+++ Part3.py
@@ -15,37 +15,59 @@ def binary_search_mod(A, x):
     right = len(A) - 1
     
     while left <= right:
+        
     
         #print(right)
         middle = int((left + right)/2)
         #print(middle)
         
+        #Found it
         if A[middle] == x:
             return middle
-        elif A[middle] < x:
-            
-            if x > A[right] and A[right] > middle:
-                #search left
+        
+        
+        if A[middle] >= A[left] and A[middle] <= A[right]:
+            #Default List. Doing regular binary search
+            if x < A[middle]:
+                #Search left side
                 right = middle - 1
             else:
-                #search right
-                left = middle +1
-        elif A[middle] > x:
-            
-            if x < A[0] and middle > A[0]:
-                #search right
+                #Search right side
+                left = middle + 1
+        elif A[middle] <= A[left] and A[middle] <= A[right]:
+            #Case 2
+            if x > A[middle] and x <= A[right]:
+                #Search Right. Its in order so we know what range it can contain
                 left = middle + 1
             else:
-                #search left
+                #Search Left. This side contains the split
                 right = middle - 1
+        elif A[middle] >= A[left] and A[middle] >= A[left]:
+            #Case 3
+            if x < A[middle] and x >= A[left]:
+                #Search left. Its in order so we know what range it can contain
+                right = middle - 1
+            else:
+                #Search right. This side contains the split
+                left = middle + 1
+        else:
+            print("Error")
+            #Should never reach here
+            
+    return "Did not find value!"
                 
           
     
 #Testing function
 def test():
-    test1 = binary_search_mod([10, 1, 2, 3, 4, 5, 6, 7, 8], 6)
-    test2 = binary_search_mod([3,4,5,6,7,8,9, 10, 1,2], 10)
-    print(test1)
-    print(test2)
+    #test1 = binary_search_mod([10, 1, 2, 3, 4, 5, 6, 7, 8], 6)
+    #test2 = binary_search_mod([3,4,5,6,7,8,9, 10, 1,2], 10)
+    test3 = binary_search_mod([24, 25, 26, 27, 28, 29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23],24)
+    #print(test1)
+    #print(test2)
+    print(test3)
     
 test()
+
+
+    
